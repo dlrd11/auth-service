a744c02f9e96c25c543a2dfb44894d84210be3dd
DeisyRomero <mayo19dr@hotmail.com>
2024-07-07 13:08:50 -0500

    :white_check_mark: adding tests

--- models.py
+++ models.py
@@ -1,18 +1,20 @@
-from sqlalchemy import Column, String, Integer
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 from passlib.context import CryptContext
-from database import Base
 
-pwd_contest = CryptContext(schemes=["bcrypt"], deprecated="auto")
+Base = declarative_base()
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 class User(Base):
-    __tablename__ = "users"
+    __tablename__ = 'users'
+
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String, unique=True, index=True)
-    hashed_password = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
 
-    def set_password(self, password):
-        self.hashed_password = pwd_contest.encrypt(password)
+    def set_password(self, password: str):
+        self.hashed_password = pwd_context.hash(password)
 
-    def verify_password(self, password) -> bool:
-        return pwd_contest.verify(password, self.hashed_password)
+    def verify_password(self, password: str) -> bool:
+        return pwd_context.verify(password, self.hashed_password)
