655ff4287df910ac4231e3a3af678c13d581d7cc
DeisyRomero <mayo19dr@hotmail.com>
2024-07-19 08:22:44 -0500

    :adhesive_bandage: updating the urls and their functionalities

--- auth.py
+++ auth.py
@@ -3,22 +3,32 @@ from typing import Optional
 from jose import JWTError, jwt
 from fastapi import Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer
+from passlib.context import CryptContext
 from sqlalchemy.orm import Session
 from models import User
 from database import get_db
 
 SECRET_KEY = "your_secret_key"
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+ACCESS_TOKEN_EXPIRE_MINUTES = 60000
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
 
 
 def authenticate_user(db: Session, username: str, password: str):
     user = db.query(User).filter(User.username == username).first()
-    if user and user.verify_password(password):
-        return user
-    return None
+    if not user or not verify_password(password, user.hashed_password):
+        return False
+    return user
 
 
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
